df9aab4de927accb813fe3f3588a999d67e03060
Mark Rendle <mark@markrendle.net>
2012-02-24 14:20:28 +0000

    Initial commit.

---
+++ Simple.Web/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Simple.Web
+{
+    using System;
+    using System.Web;
+
+    /// <summary>
+    /// Placeholder. Coolness will go here.
+    /// </summary>
+    public class SimpleWebHttpHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReusable
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
