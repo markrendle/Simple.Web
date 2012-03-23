ec2d5e43481344f93c29db99246c8f96dc631f92
Mark Rendle <mark@markrendle.net>
2012-03-23 16:59:48 +0000

    Added comments to IRedirect

--- Simple.Web/IRedirect.cs
+++ Simple.Web/IRedirect.cs
@@ -1,7 +1,13 @@
 namespace Simple.Web
 {
+    /// <summary>
+    /// Represents an endpoint that may respond with a 301, 302, 303 or 307 Redirect status.
+    /// </summary>
     public interface IRedirect
     {
+        /// <summary>
+        /// Gets the Location value to be added to the Response header.
+        /// </summary>
         string Location { get; }
     }
 }
