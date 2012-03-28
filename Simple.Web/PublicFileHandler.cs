68de709fd1bb933e5baea9b2f22c1bfe057c5955
Mark Rendle <mark@markrendle.net>
2012-03-28 18:19:55 +0100

    Optimisation for PublicFileHandler

--- Simple.Web/PublicFileHandler.cs
+++ Simple.Web/PublicFileHandler.cs
@@ -1,9 +1,12 @@
 ﻿namespace Simple.Web
 {
     using System;
+    using System.Collections.Concurrent;
+    using System.IO;
     using System.Linq;
     sealed class PublicFileHandler
     {
+        private readonly ConcurrentDictionary<string,string> _knownStaticFiles = new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
         private readonly IConfiguration _configuration;
         private readonly IWebEnvironment _environment;
 
@@ -20,27 +23,47 @@
         public bool TryHandleAsFile(Uri uri, IResponse response)
         {
             string file;
-            if (_configuration.PublicFileMappings.ContainsKey(uri.AbsolutePath))
+            if (!_knownStaticFiles.TryGetValue(uri.AbsolutePath, out file))
             {
-                file = _environment.PathUtility.MapPath(_configuration.PublicFileMappings[uri.AbsolutePath]);
-            }
-            else if (_configuration.PublicFolders.Any(folder => uri.AbsolutePath.StartsWith(folder, StringComparison.OrdinalIgnoreCase)))
-            {
-                file = _environment.PathUtility.MapPath(uri.AbsolutePath);
-            }
-            else
-            {
-                return false;
+                file = ValidateFile(uri.AbsolutePath);
+                if (file == null) return false;
             }
 
-            if (_environment.FileUtility.Exists(file))
+            try
             {
                 response.StatusCode = 200;
                 response.TransmitFile(file);
                 return true;
             }
-
+            catch (FileNotFoundException)
+            {
+                _knownStaticFiles.TryRemove(uri.AbsolutePath, out file);
+            }
             return false;
+        }
+
+        private string ValidateFile(string absolutePath)
+        {
+            string file;
+            if (_configuration.PublicFileMappings.ContainsKey(absolutePath))
+            {
+                file = _environment.PathUtility.MapPath(_configuration.PublicFileMappings[absolutePath]);
+            }
+            else if (
+                _configuration.PublicFolders.Any(
+                    folder => absolutePath.StartsWith(folder, StringComparison.OrdinalIgnoreCase)))
+            {
+                file = _environment.PathUtility.MapPath(absolutePath);
+            }
+            else
+            {
+                return null;
+            }
+            if (_environment.FileUtility.Exists(file))
+            {
+                _knownStaticFiles.TryAdd(absolutePath, file);
+            }
+            return file;
         }
     }
 
