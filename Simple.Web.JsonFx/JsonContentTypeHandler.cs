151e36bd6c9879797757d811fe242849f526ca28
Mark Rendle <mark@markrendle.net>
2012-04-23 00:24:20 +0100

    Case-insensitivity for JSON

--- Simple.Web.JsonFx/JsonContentTypeHandler.cs
+++ Simple.Web.JsonFx/JsonContentTypeHandler.cs
@@ -7,13 +7,29 @@ namespace Simple.Web.JsonFx
 {
     using System.IO;
     using global::JsonFx.Json;
+    using global::JsonFx.Json.Resolvers;
+    using global::JsonFx.Serialization;
+    using global::JsonFx.Serialization.Resolvers;
+    using global::JsonFx.Xml.Resolvers;
 
     [ContentTypes(ContentType.Json)]
     public class JsonContentTypeHandler : IContentTypeHandler
     {
         public object Read(StreamReader streamReader, Type inputType)
         {
-            return new JsonReader().Read(streamReader, inputType);
+            var resolver = new CombinedResolverStrategy(
+                new JsonResolverStrategy(),                                                             // simple JSON attributes
+                new DataContractResolverStrategy(),                                                     // DataContract attributes
+                new XmlResolverStrategy(),                                                              // XmlSerializer attributes
+                new ConventionResolverStrategy(ConventionResolverStrategy.WordCasing.PascalCase),       // DotNetStyle
+                new ConventionResolverStrategy(ConventionResolverStrategy.WordCasing.CamelCase),        // jsonStyle
+                new ConventionResolverStrategy(ConventionResolverStrategy.WordCasing.Lowercase, "-"),   // xml-style
+                new ConventionResolverStrategy(ConventionResolverStrategy.WordCasing.Uppercase, "_"));  // CONST_STYLE
+
+            // pass the combined resolver strategy into the settings object
+            var reader = new JsonReader(new DataReaderSettings(resolver));
+
+            return reader.Read(streamReader, inputType);
         }
 
         public void Write(IContent content, TextWriter textWriter)
