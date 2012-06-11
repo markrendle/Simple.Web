75ddbb4775e3aece86fb5697977796a02530a5ee
Roy Jacobs <roy.jacobs@gmail.com>
2012-06-11 10:39:10 +0200

    Added StructureMap support

---
+++ Simple.Web.StructureMap/StructureMapContainer.cs
@@ -0,0 +1,27 @@
+﻿using Simple.Web.DependencyInjection;
+using StructureMap;
+
+namespace Simple.Web.StructureMap
+{
+    public class StructureMapContainer : ISimpleContainer
+    {
+        private readonly IContainer _container;
+
+        internal StructureMapContainer(IContainer container)
+        {
+            _container = container;
+        }
+
+        public T Get<T>()
+        {
+            if (typeof(T).IsAbstract || typeof(T).IsInterface)
+            {
+                return _container.TryGetInstance<T>();
+            }
+            else
+            {
+                return _container.GetInstance<T>();
+            }
+        }
+    }
+}
