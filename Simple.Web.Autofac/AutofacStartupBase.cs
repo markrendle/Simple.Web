a7c2156b9800e6b503b9a2abb9101ec1b2995e31
rkevinstout <rkevinstout@gmail.com>
2012-06-23 23:08:55 -0500

    added Autofac support

---
+++ Simple.Web.Autofac/AutofacStartupBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple.Web.Autofac
+{
+    using global::Autofac;
+
+    public abstract class AutofacStartupBase : IStartupTask
+    {
+        public void Run(IConfiguration configuration, IWebEnvironment environment)
+        {
+            configuration.Container = new AutofacContainer(BuildContainer());
+        }
+
+        internal protected abstract IContainer BuildContainer();
+    }
+}
---
+++ Tests/Simple.Web.Autofac.Tests/HandlerFactoryBuilderTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+
+namespace Simple.Web.Autofac.Tests
+{
+    using CodeGeneration;
+    using Xunit;
+
+    public class HandlerFactoryBuilderTests
+    {
+        [Fact]
+        public void CreatesInstanceOfType()
+        {
+            var startup = new TestStartup();
+            startup.Run(SimpleWeb.Configuration, SimpleWeb.Environment);
+            var target = new HandlerBuilderFactory(SimpleWeb.Configuration);
+            var actualFunc = target.BuildHandlerBuilder(typeof(TestHandler));
+            var actual = (TestHandler)actualFunc(new Dictionary<string, string> { { "TestProperty", "Foo" } }).Handler;
+            Assert.Equal(Status.OK, actual.Get());
+            Assert.Equal("Foo", actual.TestProperty);
+        }
+
+        [Fact]
+        public void DisposesInstances()
+        {
+            var startup = new TestStartup();
+            startup.Run(SimpleWeb.Configuration, SimpleWeb.Environment);
+            var target = new HandlerBuilderFactory(SimpleWeb.Configuration);
+            var actualFunc = target.BuildHandlerBuilder(typeof(TestHandler));
+
+            TestHandler handler;
+            using (var scopedHandler = actualFunc(new Dictionary<string, string>()))
+            {
+                handler = (TestHandler)scopedHandler.Handler;
+                Assert.Equal(false, handler.IsDisposed);
+            }
+            Assert.Equal(true, handler.IsDisposed);
+        }
+    }
+
+    public class TestStartup : AutofacStartupBase
+    {
+        protected override IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //builder.RegisterType<TestHandler>()
+            //    .AsSelf()
+            //    .InstancePerLifetimeScope();
+
+            builder.RegisterType<OkResult>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+    }
+
+    public class TestHandler : IGet, IDisposable
+    {
+        private readonly IResult _result;
+        public bool IsDisposed { get; set; }
+
+        public TestHandler(IResult result)
+        {
+            _result = result;
+        }
+
+        public Status Get()
+        {
+            return _result.Result;
+        }
+
+        public string TestProperty { get; set; }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+
+    public interface IResult
+    {
+        Status Result { get; }
+    }
+
+    public class OkResult : IResult
+    {
+        public Status Result { get { return Status.OK; }}
+    }
+}
