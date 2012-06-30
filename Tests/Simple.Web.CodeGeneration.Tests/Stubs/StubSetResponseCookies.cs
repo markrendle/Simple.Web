16806489928a20747521844e284f56fdaac005d1
Iain Ballard <ieb@me.com>
2012-06-30 22:35:59 +0100

    Removing old style cookie interfaces

--- Tests/Simple.Web.CodeGeneration.Tests/Stubs/StubSetResponseCookies.cs
+++ Tests/Simple.Web.CodeGeneration.Tests/Stubs/StubSetResponseCookies.cs
@@ -3,22 +3,6 @@ namespace Simple.Web.CodeGeneration.Tests.Stubs
     using Behaviors;
     using Http;
 
-    class StubSetResponseCookies
-    {
-        public static bool Called;
-        public static bool Impl(ISetCookies e, IContext c)
-        {
-            return Called = true;
-        }
-    }
-    class StubSetRequestCookies
-    {
-        public static bool Called;
-        public static bool Impl(IReadCookies e, IContext c)
-        {
-            return Called = true;
-        }
-    }
     class StubSetUserCookie
     {
         public static bool Called;
