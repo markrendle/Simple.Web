2735766d40a2c7415e0f09a7021f858b781c9f44
Ian Battersby <ian.battersby@gmail.com>
2012-07-28 12:24:50 +0100

    Let's remove issues with case-sensitive capitalisation of FAKE params.

--- fake.sh
+++ fake.sh
@@ -3,7 +3,10 @@ TARGET=$1
 
 if [ -z "$TARGET" ]
 	then
-	TARGET="Default"
+	CTARGET="Default"
+else
+	CTARGET=`echo ${TARGET:0:1} | tr "[:lower:]" "[:upper:]"``echo ${TARGET:1} | tr "[:upper:]" "[:lower:]"`
 fi
 
-mono packages/FAKE.1.64.6/tools/FAKE.exe build.fsx target=$TARGET
+echo "Executing command: $CTARGET"
+mono packages/FAKE.1.64.6/tools/FAKE.exe build.fsx target=$CTARGET
