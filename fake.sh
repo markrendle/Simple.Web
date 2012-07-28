0481b92e546197520e070a4225bd1158f201cd53
Ian Battersby <ian.battersby@gmail.com>
2012-07-28 12:04:08 +0100

    Updated to take parameters, and target to default to DEFAULT.

--- fake.sh
+++ fake.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mono packages/FAKE.1.64.6/tools/FAKE.exe build.fsx target=Default
+TARGET=$1 
+
+if [ -z "$TARGET" ]
+	then
+	TARGET="Default"
+fi
+
+mono packages/FAKE.1.64.6/tools/FAKE.exe build.fsx target=$TARGET
