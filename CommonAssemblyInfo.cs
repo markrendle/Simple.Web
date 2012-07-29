ba5626638e2c4dc8e07ba124ef717b2a57ff45ca
markrendle <mark@markrendle.net>
2012-07-30 00:33:23 +0100

    Mucked up previous release; go to 0.4.1.2 instead.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.4.1.1")]
-[assembly: AssemblyFileVersion("0.4.1.1")]
+[assembly: AssemblyVersion("0.4.1.2")]
+[assembly: AssemblyFileVersion("0.4.1.2")]
