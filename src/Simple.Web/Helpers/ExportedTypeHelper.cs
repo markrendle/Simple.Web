31b2a0398b8be87f03ce45d676f3b6e2201316b6
ianbattersby <ian.battersby@gmail.com>
2013-07-10 15:25:33 +0100

    Better handle missing refs of ExportedTypes.

--- src/Simple.Web/Helpers/ExportedTypeHelper.cs
+++ src/Simple.Web/Helpers/ExportedTypeHelper.cs
@@ -3,6 +3,8 @@ namespace Simple.Web.Helpers
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Emit;
 
     /// <summary>
     /// For working with type reflection at runtime.
@@ -14,16 +16,27 @@ namespace Simple.Web.Helpers
         /// </summary>
         /// <param name="predicate">A predicate to filter the types.</param>
         /// <returns>A list of types.</returns>
-        public static IEnumerable<Type> FromCurrentAppDomain(Func<Type,bool> predicate)
+        public static IEnumerable<Type> FromCurrentAppDomain(Func<Type, bool> predicate)
         {
-            var list = AppDomain.CurrentDomain.GetAssemblies()
+            return AppDomain.CurrentDomain.GetAssemblies()
                 .Where(a => !(a.IsDynamic || a.GlobalAssemblyCache)) // Don't want dynamic assemblies, and if they're in the GAC they're probably BCL.
-                .Select(assembly =>
-                        assembly.GetExportedTypes().Where(predicate).ToList())
-                .SelectMany(exportedTypes => exportedTypes)
-                .ToList();
+                .SelectMany(assembly =>
+                    {
+                        Type[] types;
 
-            return list;
+                        try
+                        {
+                            types = assembly.GetExportedTypes();
+                        }
+                        catch (ReflectionTypeLoadException e) // We can't gaurantee the assembly we're exporting from as it's indirect references
+                        {
+                            types = e.Types;
+                        }
+
+                        return types;
+                    })
+                .Where(predicate)
+                .ToList();
         }
     }
 }
