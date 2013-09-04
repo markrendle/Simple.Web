849bc46a3cf16bcb7ff9991ad862e038e075433b
Eddie Garmon <eddie_garmon@hotmail.com>
2013-09-03 22:34:18 -0400

    Add IOutputEnumerable<T> support

---
+++ src/Simple.Web/Behaviors/Implementations/WriteOutputEnumerable.cs
@@ -0,0 +1,62 @@
+﻿namespace Simple.Web.Behaviors.Implementations
+{
+    using System.Collections.Generic;
+    using Behaviors;
+    using Http;
+    using MediaTypeHandling;
+
+    /// <summary>
+    /// This type supports the framework directly and should not be used from your code.
+    /// </summary>
+    public static class WriteOutputEnumerable
+    {
+        /// <summary>
+        /// This method supports the framework directly and should not be used from your code
+        /// </summary>
+        /// <typeparam name="T">The output model type.</typeparam>
+        /// <param name="handler">The handler.</param>
+        /// <param name="context">The context.</param>
+        public static void Impl<T>(IOutputEnumerable<T> handler, IContext context)
+        {
+            if (typeof (T).IsClass)
+            {
+                if (ReferenceEquals(handler.Output, null)) return;
+            }
+            WriteUsingMediaTypeHandler(handler, context);
+        }
+
+        private static void WriteUsingMediaTypeHandler<T>(IOutputEnumerable<T> handler, IContext context)
+        {
+            IMediaTypeHandler mediaTypeHandler;
+            var acceptedTypes = context.Request.GetAccept();
+            if (TryGetMediaTypeHandler(context, acceptedTypes, out mediaTypeHandler))
+            {
+                context.Response.SetContentType(mediaTypeHandler.GetContentType(acceptedTypes));
+                if (context.Request.HttpMethod.Equals("HEAD")) return;
+
+                context.Response.WriteFunction = (stream) =>
+                    {
+                        var content = new Content(context.Request.Url, handler, handler.Output);
+                        return mediaTypeHandler.Write<T>(content, stream);
+                    };
+            }
+        }
+
+        private static bool TryGetMediaTypeHandler(IContext context, IList<string> acceptedTypes,
+                                                   out IMediaTypeHandler mediaTypeHandler)
+        {
+            try
+            {
+                string matchedType;
+                mediaTypeHandler = new MediaTypeHandlerTable().GetMediaTypeHandler(acceptedTypes, out matchedType);
+            }
+            catch (UnsupportedMediaTypeException)
+            {
+                context.Response.Status = "415 Unsupported media type requested.";
+                mediaTypeHandler = null;
+                return false;
+            }
+            return true;
+        }
+    }
+}
