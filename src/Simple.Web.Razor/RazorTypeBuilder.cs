f9c9567d3548889c7ab727f81e016cb2a28bdf27
Tomas Jansson <tomas.jansson@gmail.com>
2013-09-23 22:12:43 +0200

    delete commented code

--- src/Simple.Web.Razor/RazorTypeBuilder.cs
+++ src/Simple.Web.Razor/RazorTypeBuilder.cs
@@ -162,12 +162,6 @@ namespace Simple.Web.Razor
         private static readonly IDictionary<String, String> CompilerProperties =
             new Dictionary<String, String> { { "CompilerVersion", "v4.0" } };
 
-        //private static readonly string[] ExcludedReferencesOnMono =
-        //    new[] { "System", "System.Core", "Microsoft.CSharp", "mscorlib" };
-
-        //private static readonly Func<Assembly, bool> IsValidReference = an =>
-        //        ((Type.GetType("Mono.Runtime") == null) || !ExcludedReferencesOnMono.Any(an.Location.Contains));
-
         public Type CreateType(StreamReader reader)
         {
             return CreateTypeImpl(reader);
@@ -177,7 +171,6 @@ namespace Simple.Web.Razor
         {
             var context = CreateContext(reader);
 
-//            var compilerParameters = CreateCompilerParameters(ref reader, className);
             var engine = CreateRazorTemplateEngine();
             var razorResult = engine.GenerateCode(reader, context.ClassName, engine.Host.DefaultNamespace, null);
             var viewType = CompileView(razorResult, context.GetCompilerParameters());
@@ -193,29 +186,6 @@ namespace Simple.Web.Razor
 
             return engine;
         }
-
-        //private static CompilerParameters CreateCompilerParameters(ref TextReader reader, string className)
-        //{
-        //    var compilerParameters =
-        //        new CompilerParameters()
-        //        {
-        //            GenerateExecutable = false,
-        //            GenerateInMemory = true,
-        //            TreatWarningsAsErrors = false,
-        //            OutputAssembly = Path.Combine(Path.GetTempPath(), string.Format("{0}.dll", className)),
-        //            MainClass = className
-        //        };
-
-        //    var declarationAssemblies = FindDeclarationAssemblies(ref reader);
-
-        //    compilerParameters.ReferencedAssemblies.AddRange(
-        //        TypeResolver.DefaultAssemblies
-        //        .Union(declarationAssemblies)
-        //        .Where(an => IsValidReference(an))
-        //        .Select(an => an.Location).ToArray());
-
-        //    return compilerParameters;
-        //}
 
         private static Type CompileView(GeneratorResults razorResult, CompilerParameters compilerParameters)
         {
@@ -242,32 +212,6 @@ namespace Simple.Web.Razor
             }
 
             return type;
-        }
-
-        //private static IEnumerable<Assembly> FindDeclarationAssemblies(ref TextReader reader)
-        //{
-        //    Type model;
-        //    Type handler;
-
-        //    CreateDeclarationTypes(ref reader, out handler, out model);
-
-        //    return GetNormalizedAssemblies(
-        //        new Type[] { model, handler }
-        //            .Concat(model != null && model.IsGenericType ? model.GetGenericArguments() : new Type[0]).ToArray())
-        //            .GroupBy(an => an.Location)
-        //            .Select(an => an.First())
-        //            .ToArray();
-        //}
-
-        private static IEnumerable<Assembly> GetNormalizedAssemblies(params Type[] types)
-        {
-            return from type in types where type != null select type.Assembly;
-        }
-
-        private static void CreateDeclarationTypes(ref TextReader reader, out Type handlerType, out Type modelType)
-        {
-            modelType = TypeHelper.ExtractType(ref reader, "@model");
-            handlerType = TypeHelper.ExtractType(ref reader, "@handler");
         }
     }
 
