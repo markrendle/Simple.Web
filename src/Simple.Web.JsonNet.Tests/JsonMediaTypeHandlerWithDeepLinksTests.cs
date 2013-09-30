cf5d72c6727daeaf72b236dc57916676a95d84e4
Eddie Garmon <eddie_garmon@hotmail.com>
2013-09-30 16:03:31 -0400

    oops,  missed a file for code cleanup

--- src/Simple.Web.JsonNet.Tests/JsonMediaTypeHandlerWithDeepLinksTests.cs
+++ src/Simple.Web.JsonNet.Tests/JsonMediaTypeHandlerWithDeepLinksTests.cs
@@ -3,106 +3,21 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using MediaTypeHandling;
+
     using Newtonsoft.Json.Linq;
-    using TestHelpers;
-    using TestHelpers.Sample;
+
+    using Simple.Web.MediaTypeHandling;
+    using Simple.Web.TestHelpers;
+    using Simple.Web.TestHelpers.Sample;
+
     using Xunit;
 
     public class JsonMediaTypeHandlerWithDeepLinksTests
     {
         [Fact]
-        public void SerializesCyrillicText()
-        {
-            const string russian = "Мыа алиё лаборамюз ед, ведят промпта элыктрам квюо ты.";
-            var content = new Content(new Uri("http://test.com/customer/42"), new ThingHandler(),
-                                      new Thing {Path = russian});
-            var target = new JsonMediaTypeHandlerWithDeepLinks();
-            string actual;
-            using (var stream = new StringBuilderStream())
-            {
-                target.Write<Thing>(content, stream).Wait();
-                actual = stream.StringValue;
-            }
-
-            Assert.Contains(russian, actual);
-        }
-
-        [Fact]
-        public void PicksUpOrdersLinkFromCustomer()
-        {
-            const string idProperty = @"""id"":42";
-            const string ordersLink =
-                @"{""href"":""/customer/42/orders"",""rel"":""customer.orders"",""title"":null,""type"":""application/vnd.list.order+json""}";
-            const string selfLink =
-                @"{""href"":""/customer/42"",""rel"":""self"",""title"":null,""type"":""application/vnd.customer+json""}";
-
-            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(),
-                                      new Customer {Id = 42});
-            var target = new JsonMediaTypeHandlerWithDeepLinks();
-            string actual;
-            using (var stream = new StringBuilderStream())
-            {
-                target.Write<Customer>(content, stream).Wait();
-                actual = stream.StringValue;
-            }
-            Assert.NotNull(actual);
-            Assert.Contains(idProperty, actual);
-            Assert.Contains(ordersLink, actual);
-            Assert.Contains(selfLink, actual);
-        }
-
-        [Fact]
-        public void PicksUpContactsLinkFromCustomer()
-        {
-            const string contactsLink =
-                @"{""href"":""/customer/42/contacts"",""rel"":""customer.contacts"",""title"":null,""type"":""application/vnd.contact+json""}";
-
-            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(),
-                                      new Customer {Id = 42});
-            var target = new JsonMediaTypeHandlerWithDeepLinks();
-            string actual;
-            using (var stream = new StringBuilderStream())
-            {
-                target.Write<Customer>(content, stream).Wait();
-                actual = stream.StringValue;
-            }
-            Assert.NotNull(actual);
-            Assert.Contains(contactsLink, actual);
-        }
-
-        [Fact]
-        public void PicksUpOrdersLinkFromCustomers()
-        {
-            const string idProperty = @"""id"":42";
-            const string ordersLink =
-                @"{""href"":""/customer/42/orders"",""rel"":""customer.orders"",""title"":null,""type"":""application/vnd.list.order+json""}";
-            const string selfLink =
-                @"{""href"":""/customer/42"",""rel"":""self"",""title"":null,""type"":""application/vnd.customer+json""}";
-
-            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(),
-                                      new[] {new Customer {Id = 42}});
-            var target = new JsonMediaTypeHandlerWithDeepLinks();
-            string actual;
-            using (var stream = new StringBuilderStream())
-            {
-                target.Write<IEnumerable<Customer>>(content, stream).Wait();
-                actual = stream.StringValue;
-            }
-            Assert.NotNull(actual);
-            Assert.Contains(idProperty, actual);
-            Assert.Contains(ordersLink, actual);
-            Assert.Contains(selfLink, actual);
-        }
-
-        [Fact]
         public void AddsSelfLinkToChildCollectionItems()
         {
-            var customer = new Customer
-                {
-                    Id = 42,
-                    Orders = new List<Order> {new Order {CustomerId = 42, Id = 54}}
-                };
+            var customer = new Customer { Id = 42, Orders = new List<Order> { new Order { CustomerId = 42, Id = 54 } } };
             var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(), customer);
             var target = new JsonMediaTypeHandlerWithDeepLinks();
 
@@ -127,13 +42,76 @@
         }
 
         [Fact]
+        public void PicksUpContactsLinkFromCustomer()
+        {
+            const string contactsLink =
+                @"{""href"":""/customer/42/contacts"",""rel"":""customer.contacts"",""title"":null,""type"":""application/vnd.contact+json""}";
+
+            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(), new Customer { Id = 42 });
+            var target = new JsonMediaTypeHandlerWithDeepLinks();
+            string actual;
+            using (var stream = new StringBuilderStream())
+            {
+                target.Write<Customer>(content, stream).Wait();
+                actual = stream.StringValue;
+            }
+            Assert.NotNull(actual);
+            Assert.Contains(contactsLink, actual);
+        }
+
+        [Fact]
+        public void PicksUpOrdersLinkFromCustomer()
+        {
+            const string idProperty = @"""id"":42";
+            const string ordersLink =
+                @"{""href"":""/customer/42/orders"",""rel"":""customer.orders"",""title"":null,""type"":""application/vnd.list.order+json""}";
+            const string selfLink =
+                @"{""href"":""/customer/42"",""rel"":""self"",""title"":null,""type"":""application/vnd.customer+json""}";
+
+            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(), new Customer { Id = 42 });
+            var target = new JsonMediaTypeHandlerWithDeepLinks();
+            string actual;
+            using (var stream = new StringBuilderStream())
+            {
+                target.Write<Customer>(content, stream).Wait();
+                actual = stream.StringValue;
+            }
+            Assert.NotNull(actual);
+            Assert.Contains(idProperty, actual);
+            Assert.Contains(ordersLink, actual);
+            Assert.Contains(selfLink, actual);
+        }
+
+        [Fact]
+        public void PicksUpOrdersLinkFromCustomers()
+        {
+            const string idProperty = @"""id"":42";
+            const string ordersLink =
+                @"{""href"":""/customer/42/orders"",""rel"":""customer.orders"",""title"":null,""type"":""application/vnd.list.order+json""}";
+            const string selfLink =
+                @"{""href"":""/customer/42"",""rel"":""self"",""title"":null,""type"":""application/vnd.customer+json""}";
+
+            var content = new Content(new Uri("http://test.com/customer/42"), new CustomerHandler(), new[] { new Customer { Id = 42 } });
+            var target = new JsonMediaTypeHandlerWithDeepLinks();
+            string actual;
+            using (var stream = new StringBuilderStream())
+            {
+                target.Write<IEnumerable<Customer>>(content, stream).Wait();
+                actual = stream.StringValue;
+            }
+            Assert.NotNull(actual);
+            Assert.Contains(idProperty, actual);
+            Assert.Contains(ordersLink, actual);
+            Assert.Contains(selfLink, actual);
+        }
+
+        [Fact]
         public void PicksUpPathFromThing()
         {
             const string thingLink =
                 @"{""href"":""/things?path=%2Ffoo%2Fbar"",""rel"":""self"",""title"":null,""type"":""application/json""}";
 
-            var content = new Content(new Uri("http://test.com/foo/bar"), new ThingHandler(),
-                                      new Thing {Path = "/foo/bar"});
+            var content = new Content(new Uri("http://test.com/foo/bar"), new ThingHandler(), new Thing { Path = "/foo/bar" });
             var target = new JsonMediaTypeHandlerWithDeepLinks();
             string actual;
             using (var stream = new StringBuilderStream())
@@ -143,6 +121,22 @@
             }
             Assert.NotNull(actual);
             Assert.Contains(thingLink, actual);
+        }
+
+        [Fact]
+        public void SerializesCyrillicText()
+        {
+            const string russian = "Мыа алиё лаборамюз ед, ведят промпта элыктрам квюо ты.";
+            var content = new Content(new Uri("http://test.com/customer/42"), new ThingHandler(), new Thing { Path = russian });
+            var target = new JsonMediaTypeHandlerWithDeepLinks();
+            string actual;
+            using (var stream = new StringBuilderStream())
+            {
+                target.Write<Thing>(content, stream).Wait();
+                actual = stream.StringValue;
+            }
+
+            Assert.Contains(russian, actual);
         }
     }
 }
