bfd2a40693ea1961cc8c599ace006e83f24bef2e
hoetz <flo@3dshooter.com>
2013-10-07 23:28:05 +0200

    get ExceptionHandler from IoC

---
+++ src/Tests/DefaultSimpleContainer.Test/ExceptionHandlerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefaultSimpleContainer.Test
+{
+    using Simple.Web;
+    using Simple.Web.Http;
+    using Xunit;
+
+    public class ExceptionHandlerTest
+    {
+        [Fact]
+        public void DefaultSimpleContainerChoosesExceptionHandlerWhenPresent()
+        {
+            var config = new Simple.Web.Configuration();
+            Assert.IsType<CustomExceptionHandler>(config.ExceptionHandler);
+        }
+
+        public class CustomExceptionHandler : IExceptionHandler
+        {
+            public Status Handle(Exception exception, IContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
