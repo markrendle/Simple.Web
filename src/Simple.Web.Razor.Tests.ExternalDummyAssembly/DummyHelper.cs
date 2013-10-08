f2a8ee450b4f6b38b382c5376974ea6e60b247ce
Tomas Jansson <tomas.jansson@gmail.com>
2013-10-08 11:30:57 +0200

    loading all local assemblies when building razor view

---
+++ src/Simple.Web.Razor.Tests.ExternalDummyAssembly/DummyHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Simple.Web.Razor.Tests.ExternalDummyAssembly
+{
+    public static class DummyHelper
+    {
+        public static string DummyString { get { return "Hello world!"; } }
+    }
+}
